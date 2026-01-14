359de638773c09ea710ef6b0dd74bb3918954d5e
Your Name <you@example.com>
2026-01-14 18:40:11 +0100

    Ui arbeiten

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -100,7 +100,7 @@ export const routes: Routes = [
         canActivate: [authGuard, adminGuard], // doppelt sicher
         loadComponent: () => import('./pages/admin/admin').then((m) => m.AdminPage),
         loadChildren: () =>import('./pages/admin/admin.routes').then(m => m.ADMIN_ROUTES),
-        data: { title: 'Admin Bereich' },
+        data: { title: 'Admin Bereich'},
       },
 
       // Öffentlich, aber mit Header:
