43f19adc66273cb82c6ec55cf053021b58cf06ae
Your Name <you@example.com>
2026-01-19 15:07:12 +0100

    wip

--- src/test.ts
+++ src/test.ts
@@ -1,52 +1,57 @@
 // src/test.ts
 import 'zone.js/testing';
-
-import { NgModule } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideRouter } from '@angular/router';
-import { getTestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
+
+import { provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { getApp, getApps, initializeApp as initializeFirebaseApp } from 'firebase/app';
 import { disableNetwork } from 'firebase/firestore';
 
 const firebaseTestConfig = {
-  apiKey: 'test',
-  authDomain: 'localhost',
-  projectId: 'demo-test',
-  appId: '1:123:web:123',
+  apiKey: 'demo',
+  authDomain: 'demo.firebaseapp.com',
+  projectId: 'demo',
+  appId: 'demo',
 };
 
-@NgModule({
-  imports: [BrowserDynamicTestingModule],
+const app = getApps().length ? getApp() : initializeFirebaseApp(firebaseTestConfig);
+
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  teardown: { destroyAfterEach: true },
+});
+
+// globaler Default-Provider-Setup (einmalig)
+TestBed.configureTestingModule({
+  imports: [RouterTestingModule, HttpClientTestingModule],
   providers: [
-    // RouterLink, ActivatedRoute, Router, etc.
-    provideRouter([]),
+    {
+      provide: ActivatedRoute,
+      useValue: {
+        snapshot: { params: {}, data: {}, paramMap: convertToParamMap({}) },
+        params: of({}),
+        data: of({}),
+        paramMap: of(convertToParamMap({})),
+      },
+    },
 
-    // HttpClient without real network.
-    provideHttpClient(),
-    provideHttpClientTesting(),
-
-    // AngularFire tokens used across the app.
-    provideFirebaseApp(() => initializeApp(firebaseTestConfig)),
-    provideAuth(() => getAuth()),
+    provideFirebaseApp(() => app),
+    provideAuth(() => getAuth(app)),
     provideFirestore(() => {
-      const fs = getFirestore();
-      // Prevent Firestore from attempting to talk to the network in unit tests.
-      void disableNetwork(fs).catch(() => void 0);
+      const fs = getFirestore(app);
+      disableNetwork(fs).catch(() => void 0);
       return fs;
     }),
   ],
-})
-class TestEnvironmentModule {}
-
-getTestBed().initTestEnvironment(
-  TestEnvironmentModule,
-  platformBrowserDynamicTesting()
-);
+});
